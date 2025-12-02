5e8d57ccb97b14f55fbd72bf2fe7c4f7a4a09328
bigyan1sybazzar-debug <bigyan1.sybazzar@gmail.com>
2025-12-02 10:40:59 +0545

    Update generate-sitemap.ts

--- api/generate-sitemap.ts
+++ api/generate-sitemap.ts
@@ -13,10 +13,6 @@ if (!admin.apps.length) {
 
 const db = admin.firestore();
 
-/**
- * @param {import('next').NextApiRequest} req
- * @param {import('next').NextApiResponse} res
- */
 export default async function handler(req, res) {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
